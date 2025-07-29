366b24a1ae52d13496952a48bbce24814bd37b5c
Devon <is.darksly@yahoo.com>
2025-07-28 21:05:17 -0700

    Created variables and followed the Base Pointer through the stack using GDB

---
+++ basics-4/README.md
@@ -0,0 +1,3 @@
+Managed to work with both the base pointers alongside differently sized variables. Followed the stack through and monitored its status as I descended through the memory space.
+
+Utilized intersting and new commands, like movzx, which allows us to read a smaller segment of specified memory into the 8 byte registers
--- basics-4/mem.asm
+++ basics-4/mem.asm
@@ -2,26 +2,43 @@ global _start
 
 section .text
 _start:
-	mov rdi, 5	;Sets up rdi with value of 5
+	push rbp	;8 Byte decrease: We push a garbage frame onto the stack (We will call it GF)
+	mov rbp, rsp	;We overwrite rbp with rsp, our new rbp frame
+	sub rsp, 8	;We subtract 8 bytes from rsp, reserving space for a local variable
+
+	mov qword [rbp-8], 420	;We utilize rbp as the memory point to access our variables
+	mov rdi, [rbp-8]	;Here we can watch our variable get loaded into memory
+
 	call func	;Calls func, which pushes the Memory address of the next function (Let's call it NFA in diagram) onto the stack
-	mov rax, 60	;SYSCALL exit
+	
+	mov rax, 60	;(NFA) SYSCALL exit
 	mov rdi, 0	;Status 0
 	syscall	;Syscall
 func:
-	push rbp	;Push the value of rbp (Lets call it PBP in diagram), a pointer to the start of the previous stack frame, onto the stack
+	push rbp	;8 Byte decrease: Push the value of rbp (Lets call it PBP in diagram), a pointer to the start of the previous stack frame (which holds our 420 variable), onto the stack
 	mov rbp, rsp	;Move the address of rsp (not the value at the address) into rbp, the new base pointer 
-	add rdi, 5	;Some arbitrary command to fill space
-	pop rbp	;We pop the value of rsp (Which is currently PBP) into rbp
+	
+	sub rsp, 8	;We always move in terms of 8, so push and pop dont end up at odd locations.
+	
+	mov byte [rbp-1], 69	;Here we assign our new variable, at [rbp-1]
+	mov byte [rbp-2], 42	;New variable, [rbp-2]
+	movzx rdi, byte [rbp-1]	;Loading variable [rbp-1] into rdi for viewing, movzx allows a byte to be represented in 8 byte register through zeroing
+	movzx rdi, byte [rbp-2]	;Viewing [rbp-2]
+	
+	mov rsp, rbp	;Restore the previous value of rsp
+	pop rbp	;8 Byte increase: We pop the value of rsp (Which is currently PBP) into rbp
 	ret	;We pop the value of rsp (Which is currently NFA) into RIP (the next instruction registry)
 
 ;So, the stack Looks like this throughout the program
 ;
-;Phase 0 (Pre call)	|1:---|2:---|
+;Phase 0 (First Push rbp)	|1:GF|2:---|3:---|
 ;
-;Phase 1 (call func)	|1:NFA|2:---|
+;Phase 1 (call func)	|1:GF|2:NFA|3:---|
 ;
-;Phase 2 (push rbp)	|1:NFA|2:PBP|
+;Phase 2 (push rbp)	|1:GF|2:NFA|3:PBP|
 ;
-;Phase 3 (pop rbp)	|1:NFA|2:---|
+;Phase 3 (pop rbp)	|1:GF|2:NFA|3:---|
 ;
-;Phase 4 (ret)	|1:---|2:---|
+;Phase 4 (ret)	|1:GF|2:---|3:---|
+;
+;It seems, when done correctly, we have the Garbage frame leftover
